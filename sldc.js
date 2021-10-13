f5157306355f4c915bc74d63845c6c5d4c8c0164
LevSarge <61707310+LevSarge@users.noreply.github.com>
2021-10-12 19:16:36 -0600

    Updated sldc file
    Completed definitions for variables in the file

--- sldc.js
+++ sldc.js
@@ -17,7 +17,15 @@
     Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
 */
 
-var planning = "The approach that developers take ";
-var analysisOfRequirments = "";
-var design = "";
-var Implementation = "";
+var planning =
+  "The phase where developers outine their approach to a project or product and decide on the model to which they will accomplish their objectives";
+var analysisOfRequirments =
+  "The breakdown and brainstorming of what requirements there will be to the project";
+var design =
+  "The documentation phase of the project and its features that fulfill its requirements";
+var implementation =
+  "Phase of development where features of the product are implemented according to design documents; tends to be the longest phase";
+var testingAndIntegration =
+  "Phase where bugs and defects within the programming are reported and fixes are integrated prior to deployment";
+var maintainance =
+  "The post-deployment phase that takes feedback from users and continues to refine the product";
